57c0347b634a6a3f9eacfccd3d0725373923f727
tariq50 <kamili.tariq@gmail.com>
2022-08-20 13:50:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-s6vt3p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-s6vt3p)
---
+++ index.ts
@@ -0,0 +1,9 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let mytexte1, mytexte2, mytexte3: number;
+mytexte1 = parseInt(prompt('nombre1'));
+mytexte2 = parseInt(prompt('nombre2'));
+mytexte3 = mytexte1 + mytexte2;
+console.log(mytexte3);
